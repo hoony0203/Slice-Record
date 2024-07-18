858cd03927ae503102bc7836b440c1fd78c99d6e
hoony0203 <hoony0203@naver.com>
2024-07-18 23:11:21 +0900

    section layout app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,40 @@ const App = () => {
           <section className="gap-record">
             <div className="container grid">
               <h1 className="title txt-black txt-up">
-                Kustom <br></br>Packaging & Products For Konscious <br></br>
-                Brands
+                Kustom Packaging & Products For Konscious Brands
               </h1>
               <div className="turntable"></div>
             </div>
           </section>
           <section className="gap-artist-record">
-            <div className="container grid"></div>
+            <div className="container grid ">
+              <div className="slice-record-main">
+                <h1 className="title txt-black txt-up">
+                  Kustom Packaging & Products For Konscious Brands
+                </h1>
+                <div className="menus txt-up">
+                  <div>
+                    <a href="" className="flex">
+                      <p>Genre</p>
+                      <p>01</p>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="" className="flex">
+                      <p>Artist</p>
+                      <p>02</p>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="">Favorite</a>
+                  </div>
+                  <div>
+                    <a href="">Contact</a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="turntable2"></div>
+            </div>
           </section>
         </div>
       </main>
