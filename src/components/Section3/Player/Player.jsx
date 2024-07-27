c38c8b61d6943e11a961a0d0c8c7d0f61179255e
hoony0203 <hoony0203@naver.com>
2024-07-27 09:38:13 +0900

    playerMode scrollTop

--- src/components/Section3/Player/Player.jsx
+++ src/components/Section3/Player/Player.jsx
@@ -3,12 +3,14 @@ import ArtistVideoList from "./ArtistVideoList";
 import { usePlayerStore } from "../../../store/playerStore";
 import { useEffect } from "react";
 import { useState } from "react";
+import { useRef } from "react";
 
 const Section3 = () => {
   const { listOn, playContent, playerMode } = usePlayerStore();
   const { resetVideoList } = usePlayerStore((state) => state.actions);
 
   const [opacity, setOpacity] = useState("");
+  const videoListScrollRef = useRef(null);
 
   useEffect(() => {
     let visible = setTimeout(() => {
@@ -19,6 +21,13 @@ const Section3 = () => {
       setOpacity("");
     };
   }, [listOn]);
+
+  useEffect(() => {
+    if (playerMode == true) {
+      videoListScrollRef.current.scrollTop = 0;
+    }
+  }, [playerMode]);
+
   return (
     <>
       <section className={`section-4 ${opacity}`}>
@@ -29,6 +38,7 @@ const Section3 = () => {
           <p className="txt-black txt-up txt-light-sub">{playContent}</p>
         </div>
         <div
+          ref={videoListScrollRef}
           data-lenis-prevent
           className={`turntable4 ${playerMode ? "playerMode" : null}`}>
           <ArtistVideoList />
